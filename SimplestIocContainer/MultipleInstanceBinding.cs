2b2497b5c654912743970339494f56552dc78ad9
Nikolay Lebedev <nklbdev@gmail.com>
2016-08-11 01:46:12 +0500

    Fixed swapped single and multiple instance strategies

--- SimplestIocContainer/MultipleInstanceBinding.cs
+++ SimplestIocContainer/MultipleInstanceBinding.cs
@@ -4,22 +4,13 @@ namespace SimplestIocContainer
 {
     internal class MultipleInstanceBinding : Binding
     {
-        private object _resolvedInstance;
-        private bool _isResolved;
-
         public MultipleInstanceBinding(Func<Container, object> resolver) : base(resolver)
         {
         }
 
         public override object Resolve(Container container)
         {
-            if (!_isResolved)
-            {
-                _resolvedInstance = Resolver(container);
-                _isResolved = true;
-            }
-
-            return _resolvedInstance;
+            return Resolver(container);
         }
     }
 }
--- SimplestIocContainer/SingleInstanceBinding.cs
+++ SimplestIocContainer/SingleInstanceBinding.cs
@@ -4,13 +4,22 @@ namespace SimplestIocContainer
 {
     internal class SingleInstanceBinding : Binding
     {
+        private object _resolvedInstance;
+        private bool _isResolved;
+
         public SingleInstanceBinding(Func<Container, object> resolver) : base(resolver)
         {
         }
 
         public override object Resolve(Container container)
         {
-            return Resolver(container);
+            if (!_isResolved)
+            {
+                _resolvedInstance = Resolver(container);
+                _isResolved = true;
+            }
+
+            return _resolvedInstance;
         }
     }
 }
